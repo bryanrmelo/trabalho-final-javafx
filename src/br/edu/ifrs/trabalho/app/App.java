5cb71d2edb0016458b88f6689b5a450c046d5914
Mateus Zucco <mateuszuccco@gmail.com>
2022-01-03 09:07:54 -0300

    Mudança de Tela

--- src/br/edu/ifrs/trabalho/app/App.java
+++ src/br/edu/ifrs/trabalho/app/App.java
@@ -8,8 +8,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class App extends Application {
+	private static Scene mainScene;
+	private static Scene GraphScene;
+	private static Scene ExportScene;
+	private static Scene AdcScene;
+	private static Stage stage;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -17,17 +23,44 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		Parent root;
+		Parent register;
 		try {
-			root = FXMLLoader.load(getClass().getClassLoader().getResource("views/MainPane.fxml"));
-			Stage stage = new Stage();
-			stage.setTitle("My New Stage Title");
-			stage.setScene(new Scene(root, 700, 600));
-			stage.setResizable(false);
+			stage = primaryStage;
+			Parent root = FXMLLoader.load(getClass().getResource("MainPane.fxml"));
+			mainScene = new Scene(root, 600, 550);
+			primaryStage.setScene(mainScene);
+			primaryStage.show();
+			stage.setTitle("Colocar um Titulo");
+			stage.setScene(mainScene);
 			stage.show();
+
+			register = FXMLLoader.load(getClass().getResource("GraphPane.fxml"));
+			GraphScene = new Scene(register, 620, 600);
+
+			register = FXMLLoader.load(getClass().getResource("ExportPane.fxml"));
+			ExportScene = new Scene(register, 620, 600);
+
+			register = FXMLLoader.load(getClass().getResource("AdcPane.fxml"));
+			AdcScene = new Scene(register, 620, 600);
 		}
 		catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	public static void changeScene(String scene) throws SQLException {
+		switch (scene) {
+			case "main":
+				stage.setScene(mainScene);
+				break;
+			case "graph":
+				stage.setScene(GraphScene);
+				break;
+			case "adc":
+				stage.setScene(AdcScene);
+				break;
+			case "export":
+				stage.setScene(ExportScene);
+				break;
 		}
 	}
 }
