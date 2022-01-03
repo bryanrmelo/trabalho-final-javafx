9fc21cde9cfb5f9aea021f72c32d321de7f662ec
Rafael Borges Graunke <rafael.graunke@yahoo.com>
2022-01-03 08:30:10 -0300

    Adicionado views FXML

--- src/br/edu/ifrs/trabalho/app/App.java
+++ src/br/edu/ifrs/trabalho/app/App.java
@@ -1,8 +1,33 @@
 package br.edu.ifrs.trabalho.app;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
 
 	public static void main(String[] args) {
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) {
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getClassLoader().getResource("views/MainPane.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("My New Stage Title");
+			stage.setScene(new Scene(root, 700, 600));
+			stage.setResizable(false);
+			stage.show();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
